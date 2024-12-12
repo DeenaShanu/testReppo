a439e2061570a40f794e96bc3d2b9cf98a05540d
shanu <shanu@192.168.1.38>
2024-12-12 17:54:40 +0530

    comiiited

---
+++ Maven_Project/src/main/java/maven_pack/MavenSampleCls.java
@@ -0,0 +1,17 @@
+package maven_pack;
+
+public class MavenSampleCls {
+
+	public static void main(String[] args) {
+
+		int a = -10;
+		
+		if (a > 0) {
+			System.out.println("positive");
+		} else if (a < 0) {
+			System.out.println("negative");
+		}
+
+	}
+
+}
